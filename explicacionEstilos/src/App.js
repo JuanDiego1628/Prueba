5003562ec54d69c059e94f21eabe6f376602f08f
Juan Diego Calvo h <jdcalvo@utp.edu.co>
2021-09-28 13:44:52 -0500

    hola

---
+++ explicacionEstilos/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="fondoNegro">Hola mundo</span>
+      
+
+    </div>
+  );
+}
+
+export default App;
