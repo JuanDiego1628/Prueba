4cedae6a7dffa503856382050fe5593ddb6bdb7e
Juan Diego Calvo h <jdcalvo@utp.edu.co>
2021-09-30 10:00:00 -0500

    Agrgar funcionalidad metodo1

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const vehiculosBackend=[
     {
         marca:"corolla",
@@ -31,6 +33,7 @@ const Vehiculos = () => {
     const[mostrarTabla,setMostrarTabla]=useState(false);
     const[textoBoton, setTextoBoton]=useState("Crear nuevo vehiculo");
     const[vehiculos,setVehiculos]=useState([])
+    const[colorBoton,setColorBoton]=useState("indigo")
     useEffect(() => {
         //obtener lista vehiculos backend
         setVehiculos(vehiculosBackend)
@@ -40,8 +43,10 @@ const Vehiculos = () => {
     useEffect(() => {
         if (mostrarTabla){
             setTextoBoton("Crear nuevo vehículo")
+            setColorBoton("indigo")
         }else{
             setTextoBoton("Mostrar  todos los vehículos")
+            setColorBoton("green")
         }
     }, [mostrarTabla])
     return (
@@ -53,12 +58,20 @@ const Vehiculos = () => {
                 onClick={()=>{
                     setMostrarTabla(!mostrarTabla);
                 }}
-                className="text-white bg-indigo-300 p-5 rounded-full m-6 w-28 self-end">
+                className={`text-white bg-${colorBoton}-300 p-5 rounded-full m-6 w-28 self-end`}>
                 {textoBoton}
                 </button>
             </div> 
-                {mostrarTabla? <TablaVehiculos listaVehiculos={vehiculos}/>: <FormularioCreacionVehiculos/>}
-            
+                {mostrarTabla?( <TablaVehiculos listaVehiculos={vehiculos}/>
+                ):( 
+                <FormularioCreacionVehiculos
+                fparaMostrarTabla={setMostrarTabla}
+                listaVehiculos={vehiculos}
+                fparaAgregarVehiculos={setVehiculos}
+                />)}
+            <ToastContainer
+             position="top-center"
+             autoClose={5000}/>;
         </div>
     )
 }
@@ -91,16 +104,76 @@ const TablaVehiculos=({listaVehiculos})=>{
         </table>
     )
 }
-const FormularioCreacionVehiculos=()=>{
+const FormularioCreacionVehiculos=({fparaMostrarTabla, listaVehiculos,fparaAgregarVehiculos})=>{
+    const [marca, setMarca]=useState();
+    const [color, setcolor]=useState();
+    const [modelo, setModelo]=useState();
+
+    const enviarAlBackend=()=>{
+        console.log("marca",marca);
+        toast.success("Agregado con exito")
+        fparaMostrarTabla(true)
+        fparaAgregarVehiculos([
+            ...listaVehiculos,
+            {marca:marca,color:color,modelo:modelo},
+        ])
+    }
+  
     return(
         <div className="flex flex-col justify-center items-center">
             <h2 className="text-gray-600 font-bold">Crear nuevo vehículo</h2>
-            <form className="grid grid-cols-2">
-                <input className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2" type="text"></input>
-                <input className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2" type="text"></input>
-                <input className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2" type="text"></input>
-                <input className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2" type="text"></input>
-                <button className="col-span-2 bg-green-500 p-2 rounded-full shadow-md">Guardar vhiculo</button>
+            <form className="flex flex-col">
+                <label className="flex flex-col" htmlFor="marca">
+                    Marca:
+                    <input className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2"
+                     type="text"
+                     placeholder="corolla"
+                     name="marca"
+                     value={marca}
+                     onChange={(e)=>{
+                         setMarca(e.target.value)
+                     }}
+                     ></input>
+                </label>
+                <label className="flex flex-col" htmlFor="color">
+                    Color: 
+                    <select name="color"
+                    className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2"
+                    value={color}
+                     onChange={(e)=>{
+                         setcolor(e.target.value)
+                     }}
+                    >
+                        <option disabled>Seleccione una</option>
+                        <option>Rojo</option>
+                        <option>Negro</option>
+                        <option>Verde</option>
+                    </select>
+                </label>
+                <label className="flex flex-col" htmlFor="modelo">
+                    Modelo:
+                    <input className="bg-gray-50 border border-gray-500 p-2 rounded-lg m-2"
+                     type="number"
+                     min={1992}
+                     max={2023}
+                     placeholder="corolla"
+                     name="modelo"
+                     value={modelo}
+                     onChange={(e)=>{
+                         setModelo(e.target.value)
+                     }}
+                     ></input>
+                </label>      
+                
+                <button 
+                    type="button"                
+                    className="col-span-2 bg-green-500 p-2 rounded-full shadow-md"
+                    onClick={()=>{
+                        enviarAlBackend();
+                    }}
+                >
+                Guardar vhiculo
+                </button>
             </form>
         </div>
     )
